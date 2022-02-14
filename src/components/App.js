f03e228e1771f88dd9630c78e599c7b607a578b8
dejwi <misti.krasula@gmail.com>
2022-02-14 23:49:17 +0100

    base general info & updating its data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
+import React, {Component} from 'react';
+import General from './General';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      general: {fname: 'Margaret', lname: 'Smith', email: 'yourmail@prov', phone: '643-756-123'},
+    };
+
+    this.updateGeneral = this.updateGeneral.bind(this);
+  }
+  updateGeneral(key,val) {
+    this.setState(prevState => ({
+        general: {                   // object that we want to update
+          ...prevState.general,    // keep all other key-value pairs
+          [key]: val       // update the value of specific key
+      }
+  }))
+  }
+  render() {
+    return (
+      <div>
+        <General data={this.state.general} update={this.updateGeneral}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/General.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Field from './Field';
+
+class General extends Component{
+  constructor(props){
+    super(props);
+
+    this.switchEdit = this.switchEdit.bind(this);
+    this.state = {isEdit: false};
+  }
+
+  switchEdit(e){
+    const newState = !this.state.isEdit;
+    this.setState({isEdit: newState});
+    e.target.textContent = newState ? 'Sumbit' : 'Edit';
+  }
+
+  render(){
+    const elements = Object.entries(this.props.data).map(entr => 
+      <Field defval={entr[1]} key={entr[0]} keyobj={entr[0]} isEdit={this.state.isEdit} update={this.props.update}/>);
+    return ( 
+    <div>
+      <h2>General</h2>
+      {elements}
+      <button onClick={this.switchEdit}>Edit</button>
+    </div> );
+  }
+}
+
+export default General;
